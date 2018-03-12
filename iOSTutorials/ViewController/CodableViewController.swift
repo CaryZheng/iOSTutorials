6311c8333582141ada9aa174e34c2ddcd936b8c2
CaryZheng <CaryZhengMy@gmail.com>
2018-03-12 15:40:51 +0800

    Update json encode/decode demo

--- iOSTutorials/ViewController/CodableViewController.swift
+++ iOSTutorials/ViewController/CodableViewController.swift
@@ -13,14 +13,33 @@ struct User: Codable {
     var age: Int
 }
 
+enum Gender: Int, Codable {
+    case male = 1
+    case female
+}
+
+class Location: Codable {
+    var lat: Double = 0.0
+    var lon: Double = 0.0
+}
+
+struct AdvancedUser: Codable {
+    var name: String
+    var age: Int
+    var gender: Gender
+    var location: Location
+}
+
 class CodableViewController: ZViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         testJsonStrToJsonObj()
-        
         testJsonObjToJsonStr()
+        
+        test2JsonStrToJsonObj()
+        test2JsonObjToJsonStr()
     }
     
     private func testJsonStrToJsonObj() {
@@ -45,6 +64,47 @@ class CodableViewController: ZViewController {
     
     private func testJsonObjToJsonStr() {
         let user = User(name: "Cary", age: 20)
+        
+        let encoder = JSONEncoder()
+        do {
+            let data = try encoder.encode(user)
+            let dataStr = String(data: data, encoding: .utf8)
+            print("dataStr = \(String(describing: dataStr))")
+        } catch {
+            print("Encode error")
+        }
+    }
+    
+    private func test2JsonStrToJsonObj() {
+        let jsonStr = """
+        {
+            "name": "zzb",
+            "age": 18,
+            "gender": 2,
+            "location": {
+                "lat": 99.9,
+                "lon": 87.5
+            }
+        }
+        """
+        
+        let jsonData = jsonStr.data(using: .utf8)!
+        
+        let decoder = JSONDecoder()
+        
+        do {
+            let userObj = try decoder.decode(AdvancedUser.self, from: jsonData)
+            print("userObj = \(userObj)")
+        } catch {
+            print("Decode error")
+        }
+    }
+    
+    private func test2JsonObjToJsonStr() {
+        let location = Location()
+        location.lat = 99.9
+        location.lon = 87.5
+        let user = AdvancedUser(name: "Cary", age: 19, gender: .male, location: location)
         
         let encoder = JSONEncoder()
         do {
